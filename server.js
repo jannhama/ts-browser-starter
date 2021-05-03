9fca7f31c23d11fbf4698b9d3188cd12ef6512ce
Janne Hamalainen <janne.a.hamalainen@gmail.com>
2021-05-03 08:16:14 +0300

    First route working

--- server.js
+++ server.js
@@ -1,31 +1,38 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const cors = require('cors');
 
 const api = axios.create({ baseURL: 'https://www.metaweather.com/api' });
 app.use(require('body-parser').json());
-app.use(require('cors')());
+app.use(cors());
 
 app.get('/api/location', (req, res) => {
     const { query: { search } } = req;
     console.log(req.query);
-    return api.get(`/location/search/?query=${search}`)
-        .then(({data}) =>  res.json(data))
-        .catch((e) => res.sendStatus(500));
+    return api.get(`/location/search?query=${search}`)
+    .then((response) => {
+        const {data} = response;
+        console.log(data);
+        return res.json(data);
+    })
+    .catch((e) => res.sendStatus(500));
 });
 
 app.get('/api/location/:id', (req, res) => {
     const locationId = req.params.id;
     console.log(req.id);
     return api.get(`/location/${locationId}`)
-        .then(({data}) =>  res.json(data))
+        .then(
+            ({ data }) => { res.json(data); }
+        )
         .catch((e) => res.sendStatus(500));
 });
 
 
 app.get('*', (req, res) => {
     res.json("Make sure you're using /api/location/search?query={your search term}");
-})
+});
 
 app.listen(3000, () => {
     console.log('up and running!');
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,43 @@
 console.log('hi');
 
-type response {
-  title:	string, //		Name of the location
-  location_type:	string,	//(City|Region / State / Province|Country|Continent)	
-  latt_long:	number[], // comma separated		
+type LocationItem = {
+  title: string, //	Name of the location
+  location_type: string,	// (City|Region / State / Province|Country|Continent)
+  latt_long: number[], // comma separated
   woeid: number,		// Where On Earth ID
-  distance:	number,	//Metres
-}
+  distance: number,	// Metres
+};
 
-const searchLocations = (query: string): Promise<response> => {
-  return fetch(`http://localhost/api/locations?search=$(query)`);
+export const searchLocations = async (query: string) => {
+  return fetch(`http://localhost:3000/api/location?search=${query}`);
 
-} 
+};
+
+export const doSearch = async (location: string) => {
+  const locationList: HTMLElement = document.getElementById('locationList');
+  locationList.innerHTML = '';
+  const result: LocationItem[] = await (await searchLocations(location)).json();
+
+  if (result && result.length) {
+    result.forEach(element => {
+      const newElement = `
+      <div>
+        <span>${element.title}</span><span>${element.location_type}</span><span>${element.woeid}</span>
+      </div>
+      `;
+      locationList.innerHTML += newElement;
+    });
+  }
+  console.log(result);
+  return result;
+};
+
+const element = document.getElementById('submitQuery');
+console.log(element);
+element.addEventListener('click', (e) => {
+  const locationText: string = (document.getElementById('locationText') as HTMLInputElement).value;
+  if (locationText.length) {
+    doSearch(locationText);
+  }
+
+});
